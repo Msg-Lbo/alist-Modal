f43f2c2ffb9faac8aae4f10562fe65cee1469264
绕远路的出租车司机 <63098778+Msg-Lbo@users.noreply.github.com>
2022-05-19 00:48:50 +0800

    Update modal.js

--- Modal/modal.js
+++ Modal/modal.js
@@ -19,8 +19,8 @@
         // 移动端
         judgeUA = 0
     }
-    if (judgeUA == 0 && w <= 450) {
-    // if (judgeUA == 0 && url == "https://pan.ylmty.cc/" && w <= 450) {
+    // if (judgeUA == 0 && w <= 450) {
+    if (judgeUA == 0 && url == "https://pan.ylmty.cc/" && w <= 450) {
         // url填自己的网站域名加http(s),请复制控制台输出的那一串填入这里，注意结束的/有就一起复制，没有就不加
         // console.log("移动端："+w)
         var div = document.createElement("div");
@@ -37,8 +37,8 @@
             "</div>";
         document.body.append(div);
     }
-    if (judgeUA == 1 && w >= 450) {
-    // if (judgeUA == 1 && url == "https://pan.ylmty.cc/" && w >= 450) {
+    // if (judgeUA == 1 && w >= 450) {
+    if (judgeUA == 1 && url == "https://pan.ylmty.cc/" && w >= 450) {
         // url填自己的网站域名加http(s)
         // console.log("PC端："+w)
         var div = document.createElement("div");
